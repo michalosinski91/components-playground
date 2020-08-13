afebafe88795535e7763e8e54b11eaf32e8d8eef
Michal <michalosinski91@googlemail.com>
2020-08-13 11:49:39 +0200

    BlogCards button styles fixed

--- src/components/Projects/BlogCards/Button/Button.js
+++ src/components/Projects/BlogCards/Button/Button.js
@@ -3,7 +3,11 @@ import "./Button.scss";
 
 export default function Button(props) {
   if (props.type == "dark") {
-    return <button className="btn btn--dark">{props.text}</button>;
+    return (
+      <button className="blog-card-btn blog-card-btn--dark">
+        {props.text}
+      </button>
+    );
   }
-  return <button className="btn">{props.text}</button>;
+  return <button className="blog-card-btn">{props.text}</button>;
 }
