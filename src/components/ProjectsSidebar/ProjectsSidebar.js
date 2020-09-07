11ecc5ef96fa45136dfb6b4b2ca646d96cf98c1f
Michal <michalosinski91@googlemail.com>
2020-09-07 16:12:45 +0200

    [Projects] Sidebar hide functionality added

--- src/components/ProjectsSidebar/ProjectsSidebar.js
+++ src/components/ProjectsSidebar/ProjectsSidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProjectsSidebar.scss";
 import ProjectsSidebarItem from "./ProjectsSidebarItem/ProjectsSidebarItem";
 
@@ -7,16 +7,46 @@ export default function ProjectsSidebar({
   activeProject,
   setActiveProject,
 }) {
+  const [closeSidebar, setCloseSidebar] = useState(false);
+
   return (
-    <div className="projects-sidebar">
-      {projects.map((project) => (
-        <ProjectsSidebarItem
-          key={project}
-          label={project}
-          activeProject={activeProject}
-          setActiveProject={setActiveProject}
-        />
-      ))}
+    <div
+      className={
+        closeSidebar
+          ? "projects-sidebar projects-sidebar--close"
+          : "projects-sidebar"
+      }
+    >
+      <div
+        className={
+          closeSidebar
+            ? "projects-sidebar__container projects-sidebar__container--close"
+            : "projects-sidebar__container"
+        }
+      >
+        {projects.map((project) => (
+          <ProjectsSidebarItem
+            key={project}
+            label={project}
+            activeProject={activeProject}
+            setActiveProject={setActiveProject}
+            closeSidebar={closeSidebar}
+            setCloseSidebar={setCloseSidebar}
+          />
+        ))}
+      </div>
+      <div
+        className="projects-sidebar__toggle"
+        onClick={() => setCloseSidebar(!closeSidebar)}
+      >
+        <i
+          className={
+            closeSidebar
+              ? "fas fa-angle-double-right fa-2x"
+              : "fas fa-angle-double-left fa-2x"
+          }
+        ></i>
+      </div>
     </div>
   );
 }
