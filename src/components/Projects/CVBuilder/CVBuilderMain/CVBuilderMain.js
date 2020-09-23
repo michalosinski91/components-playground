a659c7eecbf5535ecc866474f32465c1aa0afc86
Michal <michalosinski91@googlemail.com>
2020-09-23 13:46:48 +0200

    [CVBuilder] CVBuilderMain transition animations

--- src/components/Projects/CVBuilder/CVBuilderMain/CVBuilderMain.js
+++ src/components/Projects/CVBuilder/CVBuilderMain/CVBuilderMain.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./CVBuilderMain.scss";
+import { motion, AnimatePresence } from "framer-motion";
 
 import CVBuilderHeader from "../CVBuilderHeader/CVBuilderHeader";
 import CVBuilderContent from "../CVBuilderContent/CVBuilderContent";
-import CVBuilderBtn from "../CVBuilderBtn/CVBuilderBtn";
 
 export default function CVBuilderMain({
   section,
@@ -23,49 +23,33 @@ export default function CVBuilderMain({
 }) {
   return (
     <div className="cv-builder-main">
-      <CVBuilderHeader section={section} />
-      <CVBuilderContent
-        contactDetails={contactDetails}
-        setContactDetails={setContactDetails}
-        topStrengths={topStrengths}
-        setTopStrengths={setTopStrengths}
-        workExp={workExp}
-        setWorkExp={setWorkExp}
-        workGap={workGap}
-        setWorkGap={setWorkGap}
-        section={section}
-        activeSection={activeSection}
-        setActiveSection={setActiveSection}
-        progress={progress}
-        setProgress={setProgress}
-        sections={sections}
-      />
+      <AnimatePresence exitBeforeEnter initial={false}>
+        <motion.div
+          className="cv-builder-main__container"
+          key={activeSection}
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1, transition: { duration: 0.3 } }}
+          exit={{ opacity: 0, scale: 1, transition: { duration: 0.3 } }}
+        >
+          <CVBuilderHeader section={section} />
+          <CVBuilderContent
+            contactDetails={contactDetails}
+            setContactDetails={setContactDetails}
+            topStrengths={topStrengths}
+            setTopStrengths={setTopStrengths}
+            workExp={workExp}
+            setWorkExp={setWorkExp}
+            workGap={workGap}
+            setWorkGap={setWorkGap}
+            section={section}
+            activeSection={activeSection}
+            setActiveSection={setActiveSection}
+            progress={progress}
+            setProgress={setProgress}
+            sections={sections}
+          />
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
-
-/*
-<div className="cv-builder-main__nav">
-        <CVBuilderBtn
-          type="back"
-          activeSection={activeSection}
-          setActiveSection={setActiveSection}
-          progress={progress}
-          setProgress={setProgress}
-          section={section}
-          prevActiveSection={prevActiveSection}
-          sections={sections}
-        />
-        <CVBuilderBtn
-          type="continue"
-          activeSection={activeSection}
-          setActiveSection={setActiveSection}
-          progress={progress}
-          setProgress={setProgress}
-          section={section}
-          prevActiveSection={prevActiveSection}
-          sections={sections}
-        />
-      </div>
-
-      */
